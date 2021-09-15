7623c9c125a5bbe63e5946e27c3d03856058f091
Juan Cruz <juancruz.romero.dev@gmail.com>
2021-09-15 00:22:31 -0300

    Parte 1 - Crear conexión MySQL

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Del curso de bases de datos con python de código facilito.
 
 - PyMySql es el driver para python con mysql
 - Otra alternativa a las variables de entorno con "os" podemos usar la librería "python-decouple".
+- Mejor cliente MySQL: https://www.mysql.com/products/workbench/
+- Mejor cliente PostgreSQL: https://www.pgadmin.org/
--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import pymysql
-from config
+# from config
 
 if __name__ == '__main__':
-    pass
+    try:
+        connect = pymysql.Connect(host='127.0.0.1',
+                                    port=3306,
+                                    user='root',
+                                    passwd='secret',
+                                    db='test')
+        print("conectado")
+    except pymysql.err.OperationalError as err:
+        print("no pudo conectarase")
+        print(err)
