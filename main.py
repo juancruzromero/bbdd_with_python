c051dd72f05b751f64cc12af300f8ebb3e71ba1d
Juan Cruz <juancruz.romero.dev@gmail.com>
2021-09-19 03:20:43 -0300

    Parte 2 - Crear tabla

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import pymysql
 # from config
 
+DROP_TABLE_USERS = "DROP TABLE IF EXISTS users"
+
+USERS_TABLES = """CREATE TABLE users(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+"""
+
 if __name__ == '__main__':
     try:
         connect = pymysql.Connect(host='127.0.0.1',
@@ -9,6 +19,12 @@ if __name__ == '__main__':
                                     passwd='secret',
                                     db='test')
         print("conectado")
+
+        cursor = connect.cursor()
+
+        cursor.execute(DROP_TABLE_USERS)
+        cursor.execute(USERS_TABLES)
+
     except pymysql.err.OperationalError as err:
         print("no pudo conectarase")
         print(err)
